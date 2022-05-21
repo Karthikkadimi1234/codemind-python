55462c06983bacd9979856cc8c80ed0b1100d05e
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-05-21 15:26:32 +0530

    def fib(n):     a=0     b=1     print(a,b,end=' ')     for i in range (2,n):         c=a+b         a=b         b=c         print(c,end=' ') n=int(input()) fib(n)

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,11 @@
 def fib(n):
-    a,b=0,1
+    a=0
+    b=1
     print(a,b,end=' ')
-    for i in range(2,n):
+    for i in range (2,n):
         c=a+b
-        print(c,end=' ')
         a=b
         b=c
+        print(c,end=' ')
 n=int(input())
 fib(n)
