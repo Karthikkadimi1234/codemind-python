311f48480770db2240cddb6626d975a473d640bc
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-06-02 19:20:55 +0530

    t=int(input()) for i in range(t):     n=input()     c=0     for j in n:         if ord(j)>=48 and ord(j)<=57:             c+=1     if c==len(n):         print(True)     else:         print(False)

---
+++ String_decimal_.py
@@ -0,0 +1,11 @@
+t=int(input())
+for i in range(t):
+    n=input()
+    c=0
+    for j in n:
+        if ord(j)>=48 and ord(j)<=57:
+            c+=1
+    if c==len(n):
+        print(True)
+    else:
+        print(False)
