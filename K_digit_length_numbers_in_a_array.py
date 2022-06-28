220b5addbd5016cb0637c53b509de5df4006383d
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-06-29 00:08:32 +0530

    n,k=map(int,input().split()) a=list(map(int,input().split())) c=0 for i in a:     if i<0:         i=i*-1         if k==len(str(i)):             c+=1     else:          if k==len(str(i)):             c+=1 print(c)

--- K_digit_length_numbers_in_a_array.py
+++ K_digit_length_numbers_in_a_array.py
@@ -1,9 +1,12 @@
-a,b=map(int,input().split())
-c=list(map(int,input().split()))
-d=0
-for i in c:
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+c=0
+for i in a:
     if i<0:
         i=i*-1
-    if len(str(i))==b:
-        d+=1
-print(d)
+        if k==len(str(i)):
+            c+=1
+    else:
+         if k==len(str(i)):
+            c+=1
+print(c)
