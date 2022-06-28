66a3033d5da09b0412daee5451515f0ddce1fb44
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-06-28 18:44:15 +0530

    n=int(input()) s=0 t=n for i in range(1,n//2+1):     if n%i==0:         s+=i if s==t:     print(True) else:     print(False)

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,10 @@
 n=int(input())
-su=0
-for i in range(1,n):
-    if(n%i==0):
-        su+=i
-if su==n:
-    print("True")
+s=0
+t=n
+for i in range(1,n//2+1):
+    if n%i==0:
+        s+=i
+if s==t:
+    print(True)
 else:
-    print("False")
+    print(False)
