43e3d950de32cebe61b5fbcc8f493a975121b44a
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-06-29 22:15:23 +0530

    n=int(input()) a=list(map(int,input().split())) b=a[::-1] ind=0 for i in range(n):     if b[i]%2==0:         if i==0:             ind=n-1             break         else:             ind=n-i-1             break print(ind)

--- Last_even_element_index.py
+++ Last_even_element_index.py
@@ -1,7 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
-k=0
+b=a[::-1]
+ind=0
 for i in range(n):
-    if a[i]%2==0:
-        k=i
-print(k)
+    if b[i]%2==0:
+        if i==0:
+            ind=n-1
+            break
+        else:
+            ind=n-i-1
+            break
+print(ind)    
