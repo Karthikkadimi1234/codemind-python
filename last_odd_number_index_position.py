4583073042ae9399a93b8e65d1eda27e41822e21
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2022-06-29 22:09:08 +0530

    n=int(input()) a=list(map(int,input().split())) b=a[::-1] ind=0 for i in range(n):     if a[i]%2!=0:        if i==0:           ind=n-1           break        else:            ind=n-i            break print(ind)

--- last_odd_number_index_position.py
+++ last_odd_number_index_position.py
@@ -1,7 +1,14 @@
 n=int(input())
 a=list(map(int,input().split()))
-e=0
+b=a[::-1]
+ind=0
 for i in range(n):
     if a[i]%2!=0:
-        e=i
-print(e)
+       if i==0:
+          ind=n-1
+          break
+       else:
+           ind=n-i
+           break
+print(ind)
+    
